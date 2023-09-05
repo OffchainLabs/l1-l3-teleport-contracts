2ed6a24b2646547f2b46eb1ffbcef0e847058995
Henry <11198460+godzillaba@users.noreply.github.com>
2023-09-05 13:30:25 -0400

    setup hybrid project

---
+++ hardhat.config.ts
@@ -0,0 +1,10 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-foundry";
+
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.13",
+};
+
+export default config;
