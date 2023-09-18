2e8a6aa151e75356ed0d1a8f6329b5ff43579973
Henry <11198460+godzillaba@users.noreply.github.com>
2023-09-18 15:21:01 -0400

    testCallForwarder

--- test/L2ForwarderFactory.t.sol
+++ test/L2ForwarderFactory.t.sol
@@ -6,39 +6,31 @@ import {L2ForwarderFactory} from "../contracts/L2ForwarderFactory.sol";
 import {L2Forwarder} from "../contracts/L2Forwarder.sol";
 import {Beacon} from "../contracts/Beacon.sol";
 import {ClonableBeaconProxy} from "@arbitrum/token-bridge-contracts/contracts/tokenbridge/libraries/ClonableBeaconProxy.sol";
+import {AddressAliasHelper} from "@arbitrum/token-bridge-contracts/contracts/tokenbridge/libraries/AddressAliasHelper.sol";
 import {ForkTest} from "./Fork.t.sol";
+import {MockToken} from "../contracts/mocks/MockToken.sol";
 
 contract TeleporterTest is ForkTest {
     L2ForwarderFactory factory;
     Beacon beacon;
+    MockToken l2Token;
+    address predictedForwarder;
     address l1Teleporter = address(0x1100);
+    address l1Owner = address(0x2200);
 
     function setUp() public {
         beacon = new Beacon(address(new L2Forwarder()));
         factory = new L2ForwarderFactory(address(beacon), l1Teleporter);
+        l2Token = new MockToken("MOCK", "MOCK", 100 ether, address(this));
+        predictedForwarder = factory.l2ForwarderAddress(l1Owner);
     }
 
-    /// forge-config: default.fuzz.runs = 20
     function testPredictionAndActualForwarderAddresses() public {
-        address l1Owner = address(0x2200);
-        address predicted = factory.l2ForwarderAddress(l1Owner);
         address actual = address(factory.createL2Forwarder(l1Owner));
 
-        assertEq(predicted, actual);
+        assertEq(predictedForwarder, actual);
 
-        bytes memory code;
-
-        assembly {
-            let size := extcodesize(actual)
-            // allocate code byte array with new size
-            mstore(code, size)
-            // update free memory pointer
-            mstore(0x40, add(code, add(0x20, size)))
-            // copy the code
-            extcodecopy(actual, add(code, 0x20), 0, size)
-        }
-
-        assertEq(keccak256(code), keccak256(type(ClonableBeaconProxy).runtimeCode));
+        assertTrue(_isForwarder(actual));
     }
 
     function testCallForwarderAccessControl() public {
@@ -51,5 +43,52 @@ contract TeleporterTest is ForkTest {
         factory.createL2Forwarder(vm.addr(2));
     }
 
+    function testCallForwarder() public {
+        l2Token.transfer(predictedForwarder, 10 ether);
+        vm.deal(predictedForwarder, 10 ether);
 
+        address to = address(0x3300);
+
+        vm.prank(AddressAliasHelper.applyL1ToL2Alias(l1Teleporter));
+        vm.expectCall(
+            predictedForwarder,
+            0,
+            abi.encodeCall(
+                L2Forwarder.bridgeToL3,
+                (
+                    address(l2Token),
+                    address(l1l2Router),
+                    to,
+                    10 ether,
+                    1_000_000,
+                    0.1 gwei
+                )
+            )
+        );
+        factory.callForwarder({
+            l1Owner: l1Owner,
+            token: address(l2Token),
+            router: address(l1l2Router),
+            to: to,
+            amount: 10 ether,
+            gasLimit: 1_000_000,
+            gasPrice: 0.1 gwei
+        });
+    }
+
+    function _isForwarder(address addr) internal view returns (bool) {
+        uint256 size;
+
+        assembly {
+            size := extcodesize(addr)
+        }
+        
+        bytes memory code = new bytes(size);
+
+        assembly {
+            extcodecopy(addr, add(code, 0x20), 0, size)
+        }
+
+        return keccak256(code) == keccak256(type(ClonableBeaconProxy).runtimeCode);
+    }
 }
