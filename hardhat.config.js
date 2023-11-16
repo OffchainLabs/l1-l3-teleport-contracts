f85d8d0a39aa1531c537e9d5ae4006bcc7cb01e5
gzeon <im@gzeon.dev>
2023-11-17 01:19:22 +0800

    chore: change artifacts path

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,9 @@
 // we have a hardhat config so that the sdk can build and deploy our contracts
 
 module.exports = {
+  paths: {
+    artifacts: 'build/contracts',
+  },
   solidity: {
     version: "0.8.13",
     settings: {
