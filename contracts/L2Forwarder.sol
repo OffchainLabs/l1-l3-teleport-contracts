bfb5f4200b242cf42d1c47d48f78615a672631ba
gzeon <im@gzeon.dev>
2024-02-08 18:03:48 +0800

    fix: init logic

--- contracts/L2Forwarder.sol
+++ contracts/L2Forwarder.sol
@@ -20,6 +20,7 @@ contract L2Forwarder is IL2Forwarder {
 
     constructor(address _factory) {
         l2ForwarderFactory = _factory;
+        owner = 0x000000000000000000000000000000000000dEaD;
     }
 
     /// @inheritdoc IL2Forwarder
