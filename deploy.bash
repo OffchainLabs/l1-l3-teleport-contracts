32d113b6d31bdd999b6a7b5259b8169d8cc4960d
Henry <11198460+godzillaba@users.noreply.github.com>
2024-05-01 10:39:13 -0400

    update deployment script, add remappings

--- deploy.bash
+++ deploy.bash
@@ -9,9 +9,9 @@ fi
 # read environment variables from .env file
 source .env
 
-# make sure PRIVATE_KEY, ETHERSCAN_API_KEY, and ARBISCAN_API_KEY are set
-if [[ -z "$PRIVATE_KEY" || -z "$ETHERSCAN_API_KEY" || -z "$ARBISCAN_API_KEY" ]]; then
-    echo "Please set PRIVATE_KEY, ETHERSCAN_API_KEY, and ARBISCAN_API_KEY environment variables"
+# make sure PRIVATE_KEY is set
+if [[ -z "$PRIVATE_KEY" ]]; then
+    echo "Please set PRIVATE_KEY environment variable"
     exit 1
 fi
 
@@ -32,7 +32,18 @@ forge script script/0_DeployL2Contracts.s.sol --rpc-url $2
 
 # for L1
 echo "Deploying L1Teleporter..."
-forge script script/1_DeployL1Teleporter.s.sol --rpc-url $L1_URL --broadcast --verify --etherscan-api-key $ETHERSCAN_API_KEY
+L1_ETHERSCAN_API_KEY_VAR="ETHERSCAN_API_KEY_$L1_CHAIN_ID"
+L1_ETHERSCAN_API_KEY="${!L1_ETHERSCAN_API_KEY_VAR}"
+if [[ -z "$L1_ETHERSCAN_API_KEY" ]]; then
+    echo "Please set ETHERSCAN_API_KEY_$L1_CHAIN_ID environment variable."
+    exit 1
+fi
+# if DRY_RUN_DEPLOY is not "1", then broadcast and verify the deployment
+BROADCAST_ARGS=""
+if [[ "$DRY_RUN_DEPLOY" != "1" ]]; then
+    BROADCAST_ARGS="--broadcast --verify --etherscan-api-key $L1_ETHERSCAN_API_KEY"
+fi
+forge script script/1_DeployL1Teleporter.s.sol --rpc-url $L1_URL $BROADCAST_ARGS
 
 # exit if deployment failed
 if [ $? -ne 0 ]; then
@@ -44,5 +55,16 @@ fi
 for (( i=2; i<=$#; i++ )); do
     CHAIN_ID=`cast chain-id --rpc-url ${!i}`
     echo "Deploying L2 Contracts to $CHAIN_ID..."
-    forge script script/0_DeployL2Contracts.s.sol --rpc-url ${!i} --broadcast --verify --etherscan-api-key $ARBISCAN_API_KEY
+    API_KEY_VAR="ETHERSCAN_API_KEY_$CHAIN_ID"
+    API_KEY="${!API_KEY_VAR}"
+    if [[ -z "$API_KEY" ]]; then
+        echo "Please set $API_KEY_VAR environment variable."
+        exit 1
+    fi
+    # if DRY_RUN_DEPLOY is not "1", then broadcast and verify the deployment
+    BROADCAST_ARGS=""
+    if [[ "$DRY_RUN_DEPLOY" != "1" ]]; then
+        BROADCAST_ARGS="--broadcast --verify --etherscan-api-key $API_KEY"
+    fi
+    forge script script/0_DeployL2Contracts.s.sol --rpc-url ${!i} $BROADCAST_ARGS
 done
