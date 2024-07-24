a9b482a9cc0df30dbb14a575611a4d0dceac1d01
Henry <11198460+godzillaba@users.noreply.github.com>
2024-07-24 17:41:24 -0400

    more branches

--- test/halmos/L1Teleporter.t.sol
+++ test/halmos/L1Teleporter.t.sol
@@ -11,6 +11,7 @@ import {BaseTest} from "../util/Base.t.sol";
 contract L1TeleporterSymTest is SymTest, BaseTest {
     L1Teleporter teleporter;
     IERC20 l1Token;
+    IERC20 feeToken;
 
     function setUp() public override {
         super.setUp();
@@ -23,18 +24,26 @@ contract L1TeleporterSymTest is SymTest, BaseTest {
 
         l1Token = new ERC20PresetMinterPauser("TOKEN", "TOKEN");
         ERC20PresetMinterPauser(address(l1Token)).mint(address(this), 100 ether);
+
+        feeToken = new ERC20PresetMinterPauser("FEE", "FEE");
+        ERC20PresetMinterPauser(address(feeToken)).mint(address(this), 100 ether);
     }
 
     function check_teleport_no_leftovers(
         IL1Teleporter.RetryableGasParams memory gasParams,
         address receiver,
         uint256 amount,
-        address l2l3RouterOrInbox
+        address l2l3RouterOrInbox,
+        address l3FeeTokenL1Addr
     ) public {
-        // vm.assume()
+        vm.assume(
+            l3FeeTokenL1Addr == address(0) || l3FeeTokenL1Addr == address(l1Token)
+                || l3FeeTokenL1Addr == address(feeToken)
+        );
+
         IL1Teleporter.TeleportParams memory params = IL1Teleporter.TeleportParams({
             l1Token: address(l1Token),
-            l3FeeTokenL1Addr: address(0),
+            l3FeeTokenL1Addr: l3FeeTokenL1Addr,
             l1l2Router: address(ethGatewayRouter),
             l2l3RouterOrInbox: l2l3RouterOrInbox,
             to: receiver,
